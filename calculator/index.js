86512b6ce78526f2d2a7328cf7ef5767ee6b7ea7
Motoki Atarashi <motoki@Motokis-MacBook-Air-2.local>
2023-06-28 18:39:10 -0700

    calc

---
+++ calculator/index.js
@@ -0,0 +1,32 @@
+function applyOperation(op1, op2, operator) {
+  op1 = parseInt(op1);
+  op2 = parseInt(op2);
+
+
+  if (operator === "+") return op1 + op2;
+  if (operator === "-") return op1 - op2;
+  if (operator === "*") return op1 * op2;
+  if (operator === "/") return op1 / op2;
+
+
+  return NaN;
+}
+
+const op1 = document.getElementById("operator1");
+const op2 = document.getElementById("operator2");
+const answer = document.getElementById("answer");
+
+const operatorButtons = document.querySelectorAll(".operator-btn");
+
+// for (let i = 0; i < operatorButtons.length; i++) {
+//   let opElement = operatorButtons[i];
+//   opElement.addEventListener("click", function () {
+//     answer.value = applyOperation(op1.value, op2.value, opElement.innerHTML);
+//   });
+// }
+
+operatorButtons.forEach((opElement) =>
+  opElement.addEventListener("click", function () {
+    answer.value = applyOperation(op1.value, op2.value, opElement.innerHTML);
+  })
+);
